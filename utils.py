3e2f9c24b05fd7a1b10c4abad57e3b302cadecfe
arcnemesys <accelerenzo@proton.me>
2024-07-07 17:21:50 -0400

    Adding commit parser

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from os.path import join, exists, isdir
 from os import makedirs
+from collections import OrderedDict
 def repository_path(repository, *path):
     """Generate path for the repository git directory."""
     return join(repository.git_dir, *path)
@@ -26,3 +27,37 @@ def repository_directory(repository, *path, mkdir=False):
         return path
     else:
         return None
+
+def parse_commit(raw, start=0, kv_map=None):
+    if not kv_map:
+        kv_map = OrderedDict()
+    
+    space = raw.find(b' ', start)
+    newline = raw.find(b'\n', start)
+
+    if (space < 0) or (newline < space):
+        assert newline == start
+
+        kv_map[None] = raw[start+1:]
+        return kv_map
+    
+    key = raw[start:space]
+
+    end = start
+
+    while True:
+        end = raw.find(b'\n', end+1)
+
+        if raw[end+1] != ord(' '): break
+
+    value = raw[space+1:end].replace(b'\n', b'\n')
+
+    if key in kv_map:
+        if type(kv_map[key]) == list:
+            kv_map[key].append(value)
+        else:
+            kv_map[key] = [ kv_map[key], value]
+    else:
+        kv_map[key] = value
+    
+    return parse_commit(raw, start=end+1, kv_map=kv_map)
